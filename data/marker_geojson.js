92efe1759296d0fccd629448813daceb527b7ccb
Christoph Finke <christoph.finke@udo.edu>
2017-02-21 19:29:39 +0100

    Demo data

---
+++ data/marker_geojson.js
@@ -0,0 +1,49 @@
+var markers = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 6.943885787605097, 50.900649770259655 ]
+            },
+            "type": "Feature",
+            "properties": {
+                "color": "#E58100",
+                "number": "2.7",
+                "district": "Zollstock",
+                "title": "Südfriedhof",
+                "description": "1901 eröffnet. Entwurf von A. Kowallek. Die landschaftliche Anlage mit Rundwegen und abwechlungsreichen Baum- und Gehölzgruppen ist ein Beispiel für den Stil des Historismus.<br/>Darin ein einglischer, ein italienischer und ein deutscher Ehrenfriedhof, bedeutende Grabstätten von 1900 - 1940 und ein Hochkreuz. Die Einfriedung sowie die oktogonale Trauerkapelle sind 1905 - 1918 nach den Entwürfen von F. Bolte im neuromantischen Stil erbaut.",
+                "wiki": "https://de.wikipedia.org/wiki/S%C3%BCdfriedhof_%28K%C3%B6ln%29"
+            }
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 6.973374, 50.927833 ]
+            },
+            "type": "Feature",
+            "properties": {
+                "color": "#00ACF3",
+                "number": "2.1",
+                "district": "Deutz",
+                "title": "Drehbrücke",
+                "description": "Die aus der Gründerzeit des Deuter Hafens (Stadtbezirk 1) stammende Drehbrücke, ein technisches Denkmal, wurde 1907 errichtet und 1908 dem Verkehr übergeben. Ihre elektrisch gesteuerte, genietete Gitterträgerkonstruktion mit dem Steuerhäuschen aus Stahlblechen ist bis heute vollständig erhalten geblieben. Auf der von ihr ausgehenden Alfred-Schütte-Allee erreicht man das Poller Rheinufer mit dem in den 20er Jahren angelegten öffentlichen Grünbereich mit Sportanlagen",
+                "wiki": "https://de.wikipedia.org/wiki/Deutzer_Drehbr%C3%BCcke"
+            }
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 6.992620883448269, 50.9118381981778 ]
+            },
+            "type": "Feature",
+            "properties": {
+                "color": "#95C51F",
+                "number": "3.7",
+                "district": "Poll",
+                "title": "Alte Poller Schule",
+                "description": "Das um 1900 erbaute Schulhaus, Poller Hauptstraße 61-65, hat eine reich verzierte mehrfarbige Backsteinfassade."
+            }
+        },
+    ]
+};
