02ddb0b70fab26702f7b027ef2b731b2c1f0ff5e
Christoph Finke <christoph.finke@udo.edu>
2017-09-18 15:21:05 +0200

    split readme and use dev-wiki

--- copy_to_productive.sh
+++ copy_to_productive.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm -r ./productive/*
 cp -r ./page/* ./productive/
-mv ./productive/README_productive.md ./productive/README.md
 rm ./productive/assets/js/app_old.js
+git gui
+rm -r ./productive/*
